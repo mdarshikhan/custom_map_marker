ee42517f921110de6cf08969be36ba9aaa26ee6b
Mohd Arshi Khan <arsHi@bb68>
2023-10-10 00:36:02 +0530

    basic map setup and png format marker

--- app/src/main/java/com/mak/app/custommapmarker/MainActivity.kt
+++ app/src/main/java/com/mak/app/custommapmarker/MainActivity.kt
@@ -2,10 +2,57 @@ package com.mak.app.custommapmarker
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptor
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.mak.app.custommapmarker.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var mGoogleMap: GoogleMap
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setListeners()
+
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    private fun setListeners() {
+        binding.btnMarkerOnly.setOnClickListener {
+            if (mGoogleMap != null) {
+                if (this@MainActivity::mGoogleMap.isInitialized) {
+                    setCustomMarker()
+                }
+            }
+
+        }
+    }
+
+    private fun setCustomMarker() {
+        val sydney = LatLng(-34.0, 151.0)
+
+        val blackMarkerIcon : BitmapDescriptor = BitmapDescriptorFactory.fromResource(R.drawable.ic_black_marker)
+
+        val markerOptions : MarkerOptions = MarkerOptions()
+            .position(sydney)
+            .title("Title Of Marker")
+            .snippet("Snippet content")
+            .icon(blackMarkerIcon)
+
+        mGoogleMap.addMarker(markerOptions)
+        mGoogleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+
+    override fun onMapReady(map: GoogleMap) {
+        mGoogleMap = map
     }
 }
