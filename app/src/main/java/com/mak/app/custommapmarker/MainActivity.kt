ba7437970b3c450c7ace4e472607091486d630bd
Mohd Arshi Khan <arsHi@bb68>
2023-10-15 16:09:26 +0530

    Custom marker using layout file

--- app/src/main/java/com/mak/app/custommapmarker/MainActivity.kt
+++ app/src/main/java/com/mak/app/custommapmarker/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.mak.app.custommapmarker
 
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -10,7 +12,10 @@ import com.google.android.gms.maps.model.BitmapDescriptor
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.maps.android.ui.IconGenerator
 import com.mak.app.custommapmarker.databinding.ActivityMainBinding
+import com.mak.app.custommapmarker.databinding.CustomMarkerBinding
+import com.mak.app.custommapmarker.utils.AppUtils
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var binding: ActivityMainBinding
@@ -30,6 +35,15 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         binding.btnMarkerOnly.setOnClickListener {
             if (mGoogleMap != null) {
                 if (this@MainActivity::mGoogleMap.isInitialized) {
+                    setMarkerOnly()
+                }
+            }
+
+        }
+
+        binding.btnCustomMarker.setOnClickListener {
+            if (mGoogleMap != null) {
+                if (this@MainActivity::mGoogleMap.isInitialized) {
                     setCustomMarker()
                 }
             }
@@ -37,7 +51,9 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         }
     }
 
-    private fun setCustomMarker() {
+    private fun setMarkerOnly() {
+        mGoogleMap.clear()
+
         val sydney = LatLng(-34.0, 151.0)
 
         val blackMarkerIcon : BitmapDescriptor = BitmapDescriptorFactory.fromResource(R.drawable.ic_black_marker)
@@ -47,6 +63,28 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
             .title("Title Of Marker")
             .snippet("Snippet content")
             .icon(blackMarkerIcon)
+
+        mGoogleMap.addMarker(markerOptions)
+        mGoogleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+
+    private fun setCustomMarker() {
+        mGoogleMap.clear()
+        val sydney = LatLng(-34.0, 151.0)
+
+        val iconGenerator = IconGenerator(this@MainActivity);
+        iconGenerator.setBackground(null)
+//        iconGenerator.setBackground(getDrawable(R.drawable.info_window_bg))
+//        val inflatedView = View.inflate(this@MainActivity, R.layout.custom_marker, null)
+        val inflatedViewBinding = CustomMarkerBinding.inflate(layoutInflater)
+        inflatedViewBinding.tvTitle.text = "Snippet Sydney content"
+        iconGenerator.setContentView(inflatedViewBinding.root)
+
+        val markerOptions : MarkerOptions = MarkerOptions()
+            .position(sydney)
+            .title("Title Of Marker")
+            .snippet("Snippet content")
+            .icon(BitmapDescriptorFactory.fromBitmap(iconGenerator.makeIcon()))
 
         mGoogleMap.addMarker(markerOptions)
         mGoogleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
